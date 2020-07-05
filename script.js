cbf2b7e8f9717812c96233da41a35b948f45fadb
“Atessadai” <Atessa.dailami@gmail.com>
2020-07-05 19:45:55 -0400

    third commit

--- script.js
+++ script.js
@@ -8,36 +8,45 @@ function myFunction() {
 }
 
 
-const catImage = document.getElementById('cat-img');
-const catButton = document.querySelectorAll('.nav-links');
+
+const imagesz = document.getElementById('cat-img');
+const button = document.querySelectorAll('.nav-links');
 
 
-for (let i = 0; i < catButton.length; i++) {
-    catButton[i].addEventListener('click', fetchCatImage);
-}
+
+let catButton = button[0]
+let dogButton = button[1]
+let foxButton = button[2]
+catButton.addEventListener('click', fetchCatImage);
+dogButton.addEventListener('click', fetchDogImage);
+foxButton.addEventListener('click', fetchFoxImage);
+
 
 
 function fetchCatImage() {
     fetch('https://aws.random.cat/meow')
         .then(response => response.json())
         .then(data => {
-            catImage.innerHTML = `<img src="${data.file}"/>`;
+            imagesz.innerHTML = `<img src="${data.file}"/>`;
+        })
+}
+
+function fetchDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            imagesz.innerHTML = `<img src="${data.message}"/>`;
         })
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+function fetchFoxImage() {
+    fetch('https://randomfox.ca/floof/')
+        .then(response => response.json())
+        .then(data => {
+            imagesz.innerHTML = `<img src="${data.image}"/>`;
+        })
+}
 
 
 
