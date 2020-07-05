89c53e6b711d61a1f60f9d1f044ddaf4fad8d789
“Atessadai” <Atessa.dailami@gmail.com>
2020-07-05 17:29:18 -0400

    second commit

--- script.js
+++ script.js
@@ -1,53 +1,42 @@
-const url = "https://api.thecatapi.com/v1/images/search"
-
-fetch(url)
-
-    .then(res => res.json())
-    .then(convertedResponse => {
-        // let body = document.querySelector('body')
-        // let h1 = document.createElement('h1')
-        // h1.innerHTML = convertedResponse.results[0].name
-        // body.appendChild(h1)
-        let navLinks = document.querySelectorAll('.nav-links')
-        for (let i = 0; i < navLinks.length; i++) {
-            navLinks[i].addEventListener("click", handleClick)
-        }
-        function handleClick(e) {
-            ajax_get('https://api.thecatapi.com/v1/images/search', function (data) {
-
-                var html = '<img src="' + data[0]["url"] + '">';
-                document.querySelector("#image").innerHTML = html;
-
-            });
-        }
-
-
-
-    })
-    .catch(err => {
-        console.log("Something went wrong", err)
-    })
-
-
-
-function ajax_get(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log('responseText:' + xmlhttp.responseText);
-            try {
-                var data = JSON.parse(xmlhttp.responseText);
-            } catch (err) {
-                console.log(err.message + " in " + xmlhttp.responseText);
-                return;
-            }
-            callback(data);
-        }
-    };
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
+
+
+const catImage = document.getElementById('cat-img');
+const catButton = document.querySelectorAll('.nav-links');
+
+
+for (let i = 0; i < catButton.length; i++) {
+    catButton[i].addEventListener('click', fetchCatImage);
+}
+
+
+function fetchCatImage() {
+    fetch('https://aws.random.cat/meow')
+        .then(response => response.json())
+        .then(data => {
+            catImage.innerHTML = `<img src="${data.file}"/>`;
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
