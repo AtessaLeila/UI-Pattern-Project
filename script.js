5aabdc9ab408d17a991e7033bfb795052081dfbb
“Atessadai” <Atessa.dailami@gmail.com>
2020-08-16 16:16:45 -0400

    commit

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ function myFunction() {
     }
 }
 
-
+function toggleMenu() {
+    var menu = document.getElementById('myLinks');
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    }
+    else {
+        menu.style.display = "block";
+    }
+}
 
 const imagesz = document.getElementById('cat-img');
 const button = document.querySelectorAll('.nav-links');
